dd5c4a89c530e65e94b011ca0fa804cb5ca2b5b0
telepatehticman <telepatheticman13@gmail.com>
2019-06-03 22:25:51 -0600

    test decodeHamming

--- testMain.cpp
+++ testMain.cpp
@@ -14,5 +14,15 @@ int main(){
 
     std::cout << std::bitset<8>(test2[2]).to_string() << std::endl;
     std::cout << std::bitset<8>(test2[3]).to_string() << std::endl;
+
+    char testDec[2] = {(char)std::bitset<8>("11001100").to_ulong(), (char)std::bitset<8>("01010100").to_ulong()};
+    char testDec2[2] = {(char)std::bitset<8>("01001100").to_ulong(), (char)std::bitset<8>("01110100").to_ulong()};
+
+    char* testDec3 = algo::decodeHamming(testDec, 2);
+    char* testDec4 = algo::decodeHamming(testDec2, 2);
+
+    std::cout << testDec3[0] << std::endl;
+    std::cout << testDec4[0] << std::endl;
+
     return 0;
 }
