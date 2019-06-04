f118e750ac694fb06c8f4f9533f5423f95ea5efe
telepatehticman <telepatheticman13@gmail.com>
2019-06-03 22:30:54 -0600

    Test longer encode/decode

--- testMain.cpp
+++ testMain.cpp
@@ -4,25 +4,19 @@
 
 int main(){
 
-    std::cout << std::bitset<8>('b').to_string() << std::endl;
 
-    std::string testString = "ab";
+
+    std::string testString = "abcd";
 
     char* test = (char*)testString.c_str();
 
-    char* test2 = algo::encodeHamming(test, 2);
+    char* test2 = algo::encodeHamming(test, 4);
 
-    std::cout << std::bitset<8>(test2[2]).to_string() << std::endl;
-    std::cout << std::bitset<8>(test2[3]).to_string() << std::endl;
+    char* testDec = algo::decodeHamming(test2, 8);
 
-    char testDec[2] = {(char)std::bitset<8>("11001100").to_ulong(), (char)std::bitset<8>("01010100").to_ulong()};
-    char testDec2[2] = {(char)std::bitset<8>("01001100").to_ulong(), (char)std::bitset<8>("01110100").to_ulong()};
+    std::string toPrint (testDec, 4);
 
-    char* testDec3 = algo::decodeHamming(testDec, 2);
-    char* testDec4 = algo::decodeHamming(testDec2, 2);
-
-    std::cout << testDec3[0] << std::endl;
-    std::cout << testDec4[0] << std::endl;
+    std::cout << testDec << std::endl;
 
     return 0;
 }
