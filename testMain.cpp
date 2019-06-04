bd3a4efb002c1767e35965ac5bb40ad2bbee7540
telepatehticman <telepatheticman13@gmail.com>
2019-06-03 19:40:37 -0600

    test working encodeHamming

--- testMain.cpp
+++ testMain.cpp
@@ -4,5 +4,15 @@
 
 int main(){
 
+    std::cout << std::bitset<8>('a').to_string() << std::endl;
+
+    std::string testString = "ab";
+
+    char* test = (char*)testString.c_str();
+
+    char* test2 = algo::encodeHamming(test, 2);
+
+    std::cout << std::bitset<8>(test2[0]).to_string() << std::endl;
+    std::cout << std::bitset<8>(test2[1]).to_string() << std::endl;
     return 0;
 }
