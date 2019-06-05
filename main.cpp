1f71bbf0a3f7c7ae47f4437366c017683fc632c6
telepatheticman <telepatheticman13@gmail.com>
2019-06-04 18:59:23 -0700

    begin threading framework

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
+#include <thread>
 #include "Algorithms.hpp"
+#include "Utility.hpp"
 #include "win/curses.h"
 
+bool running;
+
+void sender(){
+    while (running){
+
+    }
+}
+
+void reciver(){
+    while (running){
+
+    }
+}
+
+void parser(){
+    while(running){
+
+    }
+}
+
 int main() {
+
+    util::init();
+
+    running = true;
+
     initscr();
 
+    std::thread send(sender);
+    std::thread recive(reciver);
+    std::thread parse(parser);
+
     getch();
+
+    send.join();
+    recive.join();
+    parse.join();
 
     endwin();
 
